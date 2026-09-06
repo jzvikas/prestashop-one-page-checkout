5d7b55d716038ace6db13f9608afb5fea8a9e684
Justinas Zvikas <jzvikas@gmail.com>
2026-09-06 06:06:07 +0300

    feat: add reservation storage unavailable domain error

---
+++ src/Checkout/Finalization/CheckoutFinalizationReservationUnavailable.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jzvikas\OnePageCheckout\Checkout\Finalization;
+
+use RuntimeException;
+
+final class CheckoutFinalizationReservationUnavailable extends RuntimeException
+{
+}
